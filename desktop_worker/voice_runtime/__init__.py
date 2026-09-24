8d08f9b1fa551cd843c1e56505d3ba037260eb7b
jakeharvey162-source <jakeharvey162@gmail.com>
2026-09-24 02:43:23 +0200

    Add Ash local voice runtime foundations

---
+++ desktop_worker/voice_runtime/__init__.py
@@ -0,0 +1,5 @@
+"""Optional local voice/runtime extensions for Ash.
+
+Wake-word/event ideas are adapted from ronitparikh/jarvis under the MIT License.
+See desktop_worker/THIRD_PARTY_NOTICES.md.
+"""
